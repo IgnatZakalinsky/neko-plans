12a3381caa72e6d3f90c60ae1dcc02dfeb63652a
neko <ai73a@yandex.ru>
2021-10-23 21:51:02 +0300

    add ls

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import V_1_0_Item from '../v_1_0_Item/V_1_0_Item'
 
@@ -7,11 +7,13 @@ export type V_1_0_ListItemType = {
     text?: string
 }
 
-export type V_1_0_Type = {
-    list?: V_1_0_ListItemType[]
+export type V_1_0_DataType = {
+    v_1_0?: {
+        list?: V_1_0_ListItemType[]
+    }
 }
 
-const data: { v_1_0?: V_1_0_Type } = {
+const data: V_1_0_DataType = {
     v_1_0: {
         list: [
             {
@@ -29,12 +31,127 @@ const data: { v_1_0?: V_1_0_Type } = {
     },
 }
 
+export type LocalStorageDateType = {
+    time?: number
+    title?: string
+    data?: V_1_0_DataType
+}
+
 const App = () => {
-    const renderData = data.v_1_0?.list?.map((d, i) => (
-        <V_1_0_Item data={d} key={i} />
+    const [ds, setDs] = useState<V_1_0_DataType | undefined>(data)
+    const [title, setTitle] = useState('')
+    const [ls, setLs] = useState<LocalStorageDateType[]>([])
+    const [checked, setChecked] = useState(0)
+
+    const load = () => {
+        const save = localStorage.getItem('saved-plans')
+
+        if (save) {
+            let parsed: LocalStorageDateType[] = []
+            try {
+                parsed = JSON.parse(save)
+            } catch (e) {
+                console.log('load: crashed save')
+            }
+
+            if (Array.isArray(parsed)) {
+                try {
+                    parsed.map((x: any) => x)
+
+                    setLs([
+                        { data: ds, time: Date.now(), title },
+                        ...ls,
+                        ...parsed.filter(
+                            (p) => !ls.find((l) => l.time === p.time)
+                        ),
+                    ])
+                } catch (e) {
+                    console.log('load: not array')
+                }
+            }
+        } else {
+            console.log('load: no save')
+        }
+    }
+
+    const updateV_1_0_ListItem = (data: V_1_0_ListItemType, i: number) => {
+        const newDs = {
+            ...ds,
+            v_1_0: {
+                list: ds?.v_1_0?.list?.map((l, ind) => {
+                    if (i === ind) return data
+                    else {
+                        return l
+                    }
+                }),
+            },
+        }
+        setDs(newDs)
+        if (ls[checked]) {
+            ls[checked].data = newDs
+        }
+    }
+
+    const renderData = ds?.v_1_0?.list?.map((d, i) => (
+        <V_1_0_Item
+            data={d}
+            key={i}
+            setDs={(data) => updateV_1_0_ListItem(data, i)}
+        />
     ))
 
-    return <div className="App">{renderData}</div>
+    const lsds = ls.map((l, i) => (
+        <div key={i}>
+            <span
+                onClick={() => {
+                    setDs(l.data)
+                    setTitle(l.title || '')
+                    setChecked(i)
+                }}
+            >
+                {checked === i ? '+ ' : '-- '}
+
+                {new Date(l.time || 0).toLocaleDateString() +
+                    ' ' +
+                    new Date(l.time || 0).toLocaleTimeString() +
+                    ' ' +
+                    l.title}
+            </span>
+
+            <button onClick={() => setLs(ls.filter((lo, ind) => ind !== i))}>
+                x
+            </button>
+        </div>
+    ))
+
+    console.log('ls: ', ls)
+    console.log('ds: ', ds)
+    console.log('checked: ', checked)
+
+    return (
+        <div className="App">
+            <input
+                value={title}
+                onChange={(e) => {
+                    setTitle(e.target.value)
+                    if (ls[checked]) {
+                        ls[checked].title = e.target.value
+                    }
+                }}
+            />
+            <div>{lsds}</div>
+
+            <button onClick={load}>load</button>
+            <button
+                onClick={() =>
+                    localStorage.setItem('saved-plans', JSON.stringify(ls))
+                }
+            >
+                save
+            </button>
+            {renderData}
+        </div>
+    )
 }
 
 export default App
--- src/v_1_0_Item/V_1_0_Item.tsx
+++ src/v_1_0_Item/V_1_0_Item.tsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { V_1_0_ListItemType } from '../app/App'
 
 type V_1_0_ItemPropsType = {
     data: V_1_0_ListItemType
+    setDs: (data: V_1_0_ListItemType) => void
 }
 
-const V_1_0_Item: React.FC<V_1_0_ItemPropsType> = ({ data }) => {
+const V_1_0_Item: React.FC<V_1_0_ItemPropsType> = ({ data, setDs }) => {
+    const ref = useRef<HTMLTextAreaElement | null>(null)
+
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.style.height = '1px'
+            ref.current.style.height = ref.current?.scrollHeight + 'px'
+        }
+    }, [data])
+
     return (
         <div>
-            <div>{data.title}</div>
-            <div>{data.text}</div>
+            <div>
+                title:
+                <input
+                    value={data.title || ''}
+                    onChange={(e) => setDs({ ...data, title: e.target.value })}
+                />
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+                text:
+                <textarea
+                    ref={ref}
+                    value={data.text || ''}
+                    onChange={(e) => setDs({ ...data, text: e.target.value })}
+                />
+            </div>
         </div>
     )
 }
